585b80a901073ea298ed428ffb29d1aa6cf7d570
Hugo Raynal <hugo.raynal@sterblue.com>
2020-05-17 17:20:23 +0200

    add noise on height frontieres

--- src/create-terrain-mesh.ts
+++ src/create-terrain-mesh.ts
@@ -49,6 +49,9 @@ export const createTerrainMesh = ({ vertices, indices, normal }) => {
       vec3 color;
       float baseHeight; 
       float baseBlend; 
+      float noiseFactor;
+      float noiseFrequency;
+      float noiseOffset;
     };
 
     const int maxColorCount = 6;
@@ -62,18 +65,55 @@ export const createTerrainMesh = ({ vertices, indices, normal }) => {
       return saturate((value - a) / (b - a));
     }
 
+    float random (in vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))
+                 * 43758.5453123);
+}
+
+    // 2D Noise based on Morgan McGuire @morgan3d
+    // https://www.shadertoy.com/view/4dS3Wd
+    float noise (in vec2 st) {
+        vec2 i = floor(st);
+        vec2 f = fract(st);
+
+        // Four corners in 2D of a tile
+        float a = random(i);
+        float b = random(i + vec2(1.0, 0.0));
+        float c = random(i + vec2(0.0, 1.0));
+        float d = random(i + vec2(1.0, 1.0));
+
+        // Smooth Interpolation
+
+        // Cubic Hermine Curve.  Same as SmoothStep()
+        vec2 u = f*f*(3.0-2.0*f);
+        // u = smoothstep(0.,1.,f);
+
+        // Mix 4 coorners percentages
+        return mix(a, b, u.x) +
+                (c - a)* u.y * (1.0 - u.x) +
+                (d - b) * u.x * u.y;
+    }
+
     void main() {
 
       float relativeHeight = saturate(vUv.y / strength);
 
       for (int i = 0; i < maxColorCount; i++ ) {
-        float colorStrength = inverseLerp( - colors[i].baseBlend / 2.0, colors[i].baseBlend / 2.0, relativeHeight - colors[i].baseHeight);
-        gl_FragColor = (1.0 - colorStrength) * gl_FragColor + colorStrength * vec4(colors[i].color, 1);
+
+        float computedNoise =  noise(vec2((vUv.x + colors[i].noiseOffset) * colors[i].noiseFrequency, (vUv.z + colors[i].noiseOffset) * colors[i].noiseFrequency));
+        float noiseFactor = colors[i].noiseFactor;
+        float baseBlendAddition = (computedNoise * noiseFactor - noiseFactor / 2.0);
+
+        float colorStrength = inverseLerp( 
+          - (colors[i].baseBlend - baseBlendAddition) / 2.0,
+          (colors[i].baseBlend + baseBlendAddition) / 2.0,
+          relativeHeight - colors[i].baseHeight
+        );
+        gl_FragColor = (1.0 - colorStrength) * gl_FragColor + colorStrength * vec4(colors[i].color, 1); 
       };
     }
   `;
-
-  // gl_FragColor = vec4(mix(colorA, colorB, (vUv.y - 10.0) / 10.0 ), 1);
 
   let uniforms = {
     strength: { value: 30 },
@@ -82,32 +122,51 @@ export const createTerrainMesh = ({ vertices, indices, normal }) => {
         {
           color: new THREE.Color(0x1982c4),
           baseHeight: 0,
-          baseBlend: 0.0,
+          baseBlend: 0,
+          noiseFactor: 0,
+          noiseFrequency: 1,
+          noiseOffset: 0,
         },
         {
           color: new THREE.Color(0xc2b280),
           baseHeight: 0.4,
           baseBlend: 0.01,
+          noiseFactor: 0,
+          noiseFrequency: 1,
+          noiseOffset: 0,
         },
         {
           color: new THREE.Color(0x63a375),
-          baseHeight: 0.5,
+          baseHeight: 0.45,
           baseBlend: 0.05,
+          noiseFactor: 0.1,
+          noiseFrequency: 1,
+          noiseOffset: 0,
         },
         {
           color: new THREE.Color(0x197278),
           baseHeight: 0.6,
-          baseBlend: 0.15,
+          baseBlend: 0.08,
+          noiseFactor: 0.3,
+          noiseFrequency: 0.2,
+          noiseOffset: 0,
         },
         {
           color: new THREE.Color(0xa2abab),
           baseHeight: 0.75,
           baseBlend: 0.15,
+          noiseFactor: 0.1,
+          noiseFrequency: 1,
+          noiseOffset: 0,
         },
         {
           color: new THREE.Color(0xffffff),
           baseHeight: 0.85,
-          baseBlend: 0.06,
+          baseBlend: 0.02,
+
+          noiseFactor: 0.1,
+          noiseFrequency: 1,
+          noiseOffset: 0,
         },
       ],
     },
